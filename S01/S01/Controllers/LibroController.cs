d72040eabc8bee7d21723af83fdb62e4c717e5b8
i201820189 <Giomarlimocaldas1234@gmail.com>
2020-09-11 21:25:17 -0500

    Finalizado

---
+++ S01/S01/Controllers/LibroController.cs
@@ -0,0 +1,105 @@
+﻿using S01.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace S01.Controllers
+{
+    public class LibroController : Controller
+    {
+        static List<Libro> misLibros = new List<Libro>();
+        static int n;
+
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult registrarLibro() 
+        {
+            n++;
+            ViewBag.n = n;
+            return View();
+
+        }
+
+        [HttpPost]
+        public ActionResult registrarLibro(Libro obj)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(obj);
+            }
+            misLibros.Add(obj);
+            return RedirectToAction("listadoLibros");
+
+        }
+
+        public ActionResult listadoLibros()
+        {
+
+            ViewData["Lista"] = misLibros.ToList();
+            return View();
+
+        }
+
+        public ActionResult detalleLibro(int? id)
+        {
+
+            Libro objL = misLibros.Where(e => e.codigo == id).FirstOrDefault();
+            return View(objL);
+
+        }
+
+        public ActionResult modificaLibro(int? id)
+        {
+
+            Libro objL = misLibros.Where(e => e.codigo == id).FirstOrDefault();
+            return View(objL);
+
+        }
+
+        [HttpPost]
+        public ActionResult modificaLibro(Libro objL)
+        {
+
+            if (!ModelState.IsValid)
+            {
+                return View(objL);
+            }
+            var unLibro = misLibros.Where(e => e.codigo == objL.codigo).FirstOrDefault();
+            unLibro.titulo = objL.titulo;
+            unLibro.autor = objL.autor;
+            unLibro.paginas = objL.paginas;
+            unLibro.area = objL.area;
+
+            return RedirectToAction("listadoLibros");
+
+        }
+
+        public ActionResult eliminaLibro(int? id)
+        {
+
+            Libro objL = misLibros.Where(e => e.codigo == id).FirstOrDefault();
+            misLibros.Remove(objL);
+            return View(objL);
+
+        }
+
+        [HttpPost]
+        public ActionResult eliminaLibro(Libro lib)
+        {
+           
+           Libro obj  = misLibros.Where(e => e.codigo == lib.codigo).FirstOrDefault();
+            misLibros.Remove(lib);
+          
+            return RedirectToAction("listadoLibros");
+
+        }
+
+    }
+}
---
+++ S01/S01/Models/Libro.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace S01.Models
+{
+    public class Libro
+    {
+        [DisplayName("CODIGO")]
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Ingrese el Codigo")]
+        public int codigo { get; set; }
+
+        [DisplayName("TITULO DEL LIBRO")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Ingrese el Titulo")]
+        public string titulo { get; set; }
+
+        [DisplayName("AUTOR DEL LIBRO")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Ingrese el Autor")]
+        public string autor { get; set; }
+
+        [DisplayName("NUMERO DE PAGINAS")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Ingrese el Paginas")]
+        [Range(1,int.MaxValue,ErrorMessage ="El rango es mayor a 1")]
+        public int paginas { get; set; }
+
+        [DisplayName("AREA DEL TITULO")]
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Ingrese el Area")]
+        [RegularExpression(@"^((CI)|(RE)|(AS))$", ErrorMessage ="[CI]-[RE]-[AS]")]
+        public string area { get; set; }
+
+
+    }
+}
